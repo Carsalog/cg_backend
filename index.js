d6058870fe44cbd9052758e17b244d6723341e02
DudkinON <dudkin.on@gmail.com>
2018-07-19 23:43:06 -0500

    feat: Add requrements

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.port || 3000;
 
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/middleware")(app);
+
+
 const server = app.listen(PORT, () => winston.info(`Server run on http://127.0.0.1:${PORT}`));
 
 module.exports = server;
