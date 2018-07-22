db01609cafd9032a65efcdb83e735728971ad3a9
DudkinON <dudkin.on@gmail.com>
2018-07-22 07:06:00 -0500

    feat: Add base code for testing user routes

---
+++ tests/integration/routes/users.test.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const {User} = require("../../../models/users");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const mongoose = require("mongoose");
+
+
+describe("/api/users", () => {
+
+  let server;
+  let token;
+  let user;
+  let usr;
+  let _usr;
+  let url;
+  const dataTypes = [0, 1, false, null, undefined, ""];
+
+  const createUser = async function () {
+    const _user = User({
+      firstName: usr.firstName,
+      lastName: usr.lastName,
+      email: usr.email,
+      phone: usr.phone,
+      password: await bcrypt.hash(usr.password, await bcrypt.genSalt(config.get("bcrypt.hashRounds"))),
+      su: false
+    });
+    return await _user.save();
+  };
+
+  beforeEach(async (done) => {
+    server = require("../../../index");
+    done();
+  });
+
+  afterEach(async (done) => {
+    await server.close();
+    done();
+  });
+
+
+});
