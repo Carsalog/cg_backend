16b43e923d39ebfd68dc0298f3aaf92a23d14ffe
DudkinON <dudkin.on@gmail.com>
2018-07-23 18:46:16 -0500

    refactor: index.js file now import app from loader.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
+const app = require("./loader");
 const winston = require("winston");
-const express = require("express");
-const app = express();
-const PORT = process.env.port || 3000;
-
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-require("./startup/middleware")(app);
+const config = require("config");
 
 
-const server = app.listen(PORT, () => winston.info(`Server run on http://127.0.0.1:${PORT}`));
-
-module.exports = server;
+app.listen(config.get("port"), () => winston.info(`Server run on http://127.0.0.1:${config.get("port")}`));
