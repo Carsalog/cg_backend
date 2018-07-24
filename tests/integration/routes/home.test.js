6064f5e21a3710e6ad4f937b96a6174ba8e6ed8c
DudkinON <dudkin.on@gmail.com>
2018-07-23 22:10:41 -0500

    feat: Add test case for requests on undefined route

--- tests/integration/routes/home.test.js
+++ tests/integration/routes/home.test.js
@@ -12,4 +12,12 @@ describe("home page: GET /", () => {
     expect(res.text.length > 0).toBeTruthy();
     done();
   });
+
+  it("should return status code 404 if route does not exist", async(done) => {
+    const res = await request(server).get("/test/route/that/not/exist");
+
+    expect(res.status).toBe(404);
+    expect(res.body).toHaveProperty("error");
+    done();
+  });
 });
