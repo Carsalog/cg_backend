f2f9ed693c6a08b35f78fa981b420bcfcb6ee502
DudkinON <dudkin.on@gmail.com>
2018-07-23 22:19:08 -0500

    feat: Add post method to auth router

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,23 @@ const express = require("express");
 const router = express.Router();
 
 
+router.post("/", async (req, res) => {
 
+  // Make sure  that data is valid
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({error: error.details[0].message});
+
+  const user = await getByEmail(req.body.email);
+  const msg = {error: "Invalid email or password"};
+
+  // Make sure that user with given email is not already registered
+  if (!user || !(await verify(req.body.password, user.password))) return res.status(400).send(msg);
+
+  // Prepare jwt
+  const token = user.generateAuthToken();
+
+  // Return user object to a client
+  return res.send({token: token});
+});
 
 module.exports = router;
