4837cddfe883a558d06cf58164d65c6cdaffb2b0
DudkinON <dudkin.on@gmail.com>
2018-07-24 12:07:57 -0500

    refactor: refactor home test

--- tests/integration/routes/home.test.js
+++ tests/integration/routes/home.test.js
@@ -1,23 +1,30 @@
 const request = require("supertest");
 const server = require("../../../loader");
 
+describe("Undefined routes or base html", () => {
 
-describe("home page: GET /", () => {
+  const url = "/test/route/that/not/exist";
 
-  it("should return status code 200 and HTML of the home page", async(done) => {
-    const res = await request(server).get("/");
+  describe("GET /", () => {
 
-    expect(res.status).toBe(200);
-    expect(res.headers).toHaveProperty("content-type", "text/html; charset=utf-8");
-    expect(res.text.length > 0).toBeTruthy();
-    done();
+    it("should return status code 200 and HTML of the home page", async(done) => {
+      const res = await request(server).get("/");
+
+      expect(res.status).toBe(200);
+      expect(res.headers).toHaveProperty("content-type", "text/html; charset=utf-8");
+      expect(res.text.length > 0).toBeTruthy();
+      done();
+    });
   });
 
-  it("should return status code 404 if route does not exist", async(done) => {
-    const res = await request(server).get("/test/route/that/not/exist");
+  describe("GET /*", () => {
 
-    expect(res.status).toBe(404);
-    expect(res.body).toHaveProperty("error");
-    done();
+    it("should return status code 404 if route does not exist", async(done) => {
+      const res = await request(server).get(url);
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
   });
 });
