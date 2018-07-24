b8b15a11ceea96c62935c9e116c546c14da39b0f
DudkinON <dudkin.on@gmail.com>
2018-07-23 22:56:24 -0500

    feat: Add test cases to test for auth route

--- tests/integration/routes/auth.test.js
+++ tests/integration/routes/auth.test.js
@@ -54,4 +54,69 @@ describe("/api/auth", () => {
     await user.remove();
     done();
   });
+
+  describe("POST /", () => {
+
+    it("should return 400 if credentials is undefined", async (done) => {
+
+      const res = await request(server).post("/api/auth");
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
+
+    it("should return 400 if user email have invalid format", async (done) => {
+
+      for (let i = 0; i < dataTypes.length; i++) {
+        credentials.email = dataTypes[i];
+        const res = await request(server).post("/api/auth").send(credentials);
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty("error");
+      }
+
+      done();
+    });
+
+    it("should return 400 if user password have invalid format", async (done) => {
+
+      for (let i = 0; i < dataTypes.length; i++) {
+        credentials.password = dataTypes[i];
+        const res = await request(server).post("/api/auth").send(credentials);
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty("error");
+      }
+
+      done();
+    });
+
+    it("should return 400 if user email is invalid", async (done) => {
+      credentials.email = "foo@baz.com";
+      const res = await request(server).post("/api/auth").send(credentials);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
+
+    it("should return 400 if user password is invalid", async (done) => {
+      credentials.password = "0987654321Ab";
+      const res = await request(server).post("/api/auth").send(credentials);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
+
+    it("should return 200 if user credentials is valid", async (done) => {
+
+      const res = await request(server).post("/api/auth").send(credentials);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("token");
+      done();
+    });
+  });
 });
