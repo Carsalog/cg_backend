89c9ced79f8d3e3ed16a5e0ca8d627b95ef09279
DudkinON <dudkin.on@gmail.com>
2018-08-05 05:19:38 -0500

    docs: Add a docstring to error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,10 @@
 const winston = require("winston");
 
 module.exports = function (err, req, res, next) {
+  /**
+   * Error middleware. If any route create error this middleware will pass
+   * the error to the winston, and return error message with status code 500
+   */
 
   // Add error to the log file
   winston.error(err);
