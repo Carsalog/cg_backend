ef6bd4a82563cc143e70d5079d3851587c1a2e8f
DudkinON <dudkin.on@gmail.com>
2018-08-08 06:27:53 -0500

    feat: createUser moved to utils.js

--- tests/integration/routes/makes.test.js
+++ tests/integration/routes/makes.test.js
@@ -5,50 +5,25 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const config = require("config");
 const server = require("../../../loader");
+const utils = require("./utils");
 
 
 describe("/api/makes", () => {
 
   let user;
   let token;
-  let usr;
-  let su;
   let makes;
   let make;
   let name;
   let url;
-
-  const createUser = async function () {
-    /**
-     * Creates user and returns a promise
-     * @type {User}
-     * @return Promise:
-     */
-    const _user = new User({
-      firstName: usr.firstName,
-      lastName: usr.lastName,
-      email: usr.email,
-      phone: usr.phone,
-      password: await bcrypt.hash(usr.password, await bcrypt.genSalt(config.get("bcrypt.hashRounds"))),
-      su: su
-    });
-    return _user.save();
-  };
 
   beforeEach(async (done) => {
     /**
      * Before each test defines user object creates user, and generate auth token
      * @type {{firstName: string, lastName: string, email: string, phone: string, password: string}}
      */
-    usr = {
-      firstName: "John",
-      lastName: "Doe",
-      email: "john.doe@makes.test",
-      phone: "12345678",
-      password: "12345678Ab",
-    };
-    su = true;
-    user = await createUser();
+
+    user = await utils.createUser("john.doe@makes.test", true);
     token = await user.generateAuthToken();
     done();
   });
@@ -234,6 +209,7 @@ describe("/api/makes", () => {
 
     it("should return status code 201 if make is valid", async (done) => {
 
+      name = "unique";
       const res = await prepare();
 
       expect(res.status).toBe(201);
