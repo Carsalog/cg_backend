55c42985cc077ea1e8833901a57ac951b4c67510
DudkinON <dudkin.on@gmail.com>
2018-08-09 05:44:04 -0500

    feat: Add GET, POST, PUT, and DELETE methods to State router

--- routes/states.js
+++ routes/states.js
@@ -9,4 +9,48 @@ const _ = require("lodash");
 const valid = require("../middleware/valid");
 
 
+router.get("/", validator, async (req, res) => {
+
+  res.send(await State.getByPage(req.params.page, req.params.amount));
+});
+
+router.post("/", [auth, su, valid(validate)], async (req, res) => {
+
+  const item = await State.getByName(req.body.name);
+  if (item) return res.status(200).send(item);
+
+  // Send response to a client
+  return res.status(201).send(_.pick(await State.create(req.body), ["_id", "name", "abbreviation"]));
+});
+
+router.put('/:id', [auth, su, idValidator, valid(validate)], async (req, res) => {
+
+  const state = await State.update(req.body, req.params.id);
+  if (!state) return res.status(404).send({error: "Cannot find this state"});
+
+  return res.send(_.pick(state, ["_id", "name", "abbreviation"]));
+});
+
+router.delete("/:id", [auth, su, idValidator], async (req, res) => {
+
+  // Try to find the state
+  const state = await State.findById(req.params.id);
+  if (!state) return res.status(404).send({error: "Cannot find this state"});
+
+  const name = String(state.name);
+  await state.remove();
+
+  // Send response to a client
+  return res.send({info: `State ${name} was removed`});
+});
+
+router.get("/:id", idValidator, async (req, res) => {
+
+  const item = await State.getById(req.params.id);
+  if (!item) return res.status(404).send({error: "Cannot find the State"});
+
+  // Send response to a client
+  return res.send(item);
+});
+
 module.exports = router;
