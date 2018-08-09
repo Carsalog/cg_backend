58c290c634ffff5ae8af243d339ad16c486cb7d8
DudkinON <dudkin.on@gmail.com>
2018-08-09 05:48:53 -0500

    feat: Add state router to app

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const error = require("../middleware/error");
+const states = require("../routes/states");
 const auth = require("../routes/auth");
 const users = require("../routes/users");
 const home = require("../routes/home");
@@ -20,6 +21,7 @@ module.exports = function (app) {
   app.disable('x-powered-by');
 
   // Routs
+  app.use('/api/states', states);
   app.use('/api/auth', auth);
   app.use('/api/users', users);
   app.use('/', home);
