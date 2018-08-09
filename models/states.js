dceea61e99ecfa26df39e4ca673fb43023ad329d
DudkinON <dudkin.on@gmail.com>
2018-08-09 05:37:49 -0500

    feat: Add methods to State model

--- models/states.js
+++ models/states.js
@@ -27,4 +27,42 @@ const state = new mongoose.Schema({
   }],
 });
 
+state.statics.getById = function (_id) {
+
+  return this.findById(_id).populate("cities", "name").select("-__v");
+};
+
+state.statics.getByPage = function(page, amount) {
+
+  return this.find().skip((page - 1) * amount).limit(amount).sort({name: 1}).select("-__v");
+};
+
+state.statics.getByName = function (name) {
+
+  return this.findOne({name: { "$regex": name, "$options": "i" }}).select("-__v");
+};
+
+state.statics.create = function (data) {
+
+  return this(data).save();
+};
+
+state.statics.update = async function (obj, _id) {
+
+  // Try to get a car type
+  const current = await this.findById(_id);
+  if (!current) return null;
+
+  // Update and return a car type
+  current.name = obj.name;
+  current.abbreviation = obj.abbreviation;
+  return current.save();
+};
+
+state.statics.delete = function (_id) {
+
+  return this.findByIdAndRemove(_id);
+};
+
+
 exports.State = mongoose.model(String(config.get("states.tableName")), state);
