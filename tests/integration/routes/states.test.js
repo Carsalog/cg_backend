6136bf74d4669ff79c6d06851bbf7a71392a4ef3
DudkinON <dudkin.on@gmail.com>
2018-08-09 05:29:02 -0500

    feat: Add test cases for GET on /api/states

--- tests/integration/routes/states.test.js
+++ tests/integration/routes/states.test.js
@@ -36,4 +36,51 @@ describe("/api/states", () => {
     done();
   });
 
+  describe("GET /", () => {
+    /**
+     * Test cases for GET on /api/states
+     */
+
+    beforeEach(async (done) => {
+      /**
+       * Before each test create states
+       * @type {*[]}
+       */
+      states = [
+        {name: "state1", abbreviation: "SO"},
+        {name: "state2", abbreviation: "ST"},
+        {name: "state3", abbreviation: "SR"}
+      ];
+      await State.collection.insertMany(states);
+      done();
+    });
+
+    afterEach(async (done) => {
+      /**
+       * After each test remove the states
+       */
+      await State.deleteMany({name: {$in: ["state1", "state2", "state3"]}});
+      done();
+    });
+
+    it("should return status code 200 and list of states if GET parameters isn't given", async (done) => {
+
+      const res = await request(server).get("/api/states");
+
+      expect(res.status).toBe(200);
+      expect(res.body.length >= states.length).toBeTruthy();
+      done();
+    });
+
+    it("should return only 2 states when GET parameter amount is 2", async (done) => {
+
+      const amount = 2;
+      const res = await request(server).get(`/api/states?page=1&amount=${amount}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(amount);
+      done();
+    });
+  });
+
 });
