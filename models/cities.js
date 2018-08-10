b67b0135df226a65f4e497dd8c9d0cbd5638fb1c
DudkinON <dudkin.on@gmail.com>
2018-08-10 03:56:24 -0500

    feat: Add methods to City model

--- models/cities.js
+++ models/cities.js
@@ -18,6 +18,41 @@ const city = new mongoose.Schema({
   },
 });
 
+city.statics.getById = function (_id) {
+
+  return this.findById(_id).select("-__v");
+};
+
+city.statics.getByPage = function (page, amount) {
+
+  return this.find().skip((page - 1) * amount).limit(amount).sort({name: 1}).select("-__v");
+};
+
+city.statics.create = function (data) {
+
+  return this({name: data.name, state: data.state}).save();
+};
+
+city.statics.getByName = function (name, _id) {
+
+  return this.findOne({name: { "$regex": name, "$options": "i" }, state: _id}).select("-__v");
+};
+
+city.statics.update = async function (obj, _id) {
+
+  // Try to get a city
+  const current = await this.findById(_id);
+  if (!current) return null;
+
+  // Update and return the city
+  current.name = obj.name;
+  return current.save();
+};
+
+city.statics.deleteById = function(_id) {
+
+  return this.findByIdAndRemove(_id);
+};
 
 exports.City = mongoose.model(String(config.get("cities.tableName")), city);
 
