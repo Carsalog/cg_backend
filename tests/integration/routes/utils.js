881a2cdde2c42fc92cc3aa03407a49f5f299cde7
DudkinON <dudkin.on@gmail.com>
2018-08-22 12:09:38 -0500

    feat: Add createUser function

---
+++ tests/integration/routes/utils.js
@@ -0,0 +1,16 @@
+const {User} = require("../../../models/users");
+const bcrypt = require("bcrypt");
+const config = require("config");
+
+module.exports.createUser = async function (email, su) {
+
+  return User({
+    firstName: "John",
+    lastName: "Doe",
+    email: email,
+    phone: "12345678",
+    password: await bcrypt.hash("12345678Ab", await bcrypt.genSalt(config.get("bcrypt.hashRounds"))),
+    su: su
+  }).save();
+};
+
