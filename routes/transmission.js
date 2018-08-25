59ba1be42cb45c04b3b56a430aacfe7469e0296a
DudkinON <dudkin.on@gmail.com>
2018-08-25 08:09:02 -0500

    feat: Add methods to transmission router

--- routes/transmission.js
+++ routes/transmission.js
@@ -8,6 +8,41 @@ const _ = require("lodash");
 const valid = require("../middleware/valid");
 
 
+router.get("/", async (req, res) => {
 
+  return res.send(await Transmission.get());
+});
+
+router.get("/:id", idValidator, async (req, res) => {
+
+  const item = await Transmission.getById(req.params.id);
+  if (!item) return res.status(404).send({error: "Cannot find the transmission"});
+
+  return res.send(item)
+});
+
+router.post("/", [auth, su, valid(validate)], async (req, res) => {
+
+  const item = await Transmission.getByType(req.body.type);
+  if (item) return res.send(_.pick(item, ["_id", "type"]));
+
+  return res.status(201).send(_.pick(await Transmission.add(req.body), ["_id", "type"]));
+});
+
+router.put("/:id", [auth, su, idValidator, valid(validate)], async (req, res) => {
+
+  const item = await Transmission.update(req.body.type, req.params.id);
+  if (!item) return res.status(404).send({error: "Cannot find this transmission"});
+
+  return res.send(item);
+});
+
+router.delete("/:id", [auth, su, idValidator], async (req, res) => {
+
+  const item = await Transmission.delById(req.params.id);
+  if (!item) return res.status(404).send({error: "Cannot find the transmission"});
+
+  return res.send({info: `Transmission ${item.type} was removed`});
+});
 
 module.exports = router;
