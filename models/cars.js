1e995089546b070d06059fd719670e62a21ab235
DudkinON <dudkin.on@gmail.com>
2018-09-10 23:47:11 -0500

    feat: Add methods to car model

--- models/cars.js
+++ models/cars.js
@@ -38,4 +38,50 @@ const car  = new mongoose.Schema({
   }
 });
 
+
+car.statics.create = function(car) {
+
+  return this(car).save();
+};
+
+car.statics.getById = function(_id) {
+
+  return this.findById(_id).select("-__v");
+};
+
+car.statics.getByVIN = function(vin) {
+
+  return this.findOne({vin: vin}).select("-__v");
+};
+
+car.statics.getByPage = function(page, amount) {
+  return this.find()
+    .skip((page - 1) * amount)
+    .select("-__v")
+    .limit(amount);
+};
+
+car.statics.update = function(obj, _id) {
+
+  const car = this.findById(_id);
+  if (!car) return null;
+  if (car.vin.toUpperCase() !== obj.vin.toUpperCase()) return null;
+
+  car.make = obj.make;
+  car.model = obj.model;
+  car.type = obj.type;
+  car.fuel = obj.fuel;
+  car.year = obj.year;
+
+  return car.save();
+};
+
+car.statics.delById = async function(_id) {
+
+  const item = await this.findById(_id);
+  if (!item) return null;
+
+  return item.remove();
+};
+
 exports.Car = mongoose.model(String(config.get("cars.tableName")), car);
