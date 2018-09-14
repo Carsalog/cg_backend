c490bba94222b3448b58fdb77a29094f36f42966
DudkinON <dudkin.on@gmail.com>
2018-09-14 15:40:36 -0500

    feat: Add tests suit for /api/cars endpoint

---
+++ tests/integration/routes/cars.test.js
@@ -0,0 +1,17 @@
+const {Car} = require("../../../models/cars");
+const request = require("supertest");
+const mongoose = require("mongoose");
+const server = require("../../../loader");
+const utils = require("./utils");
+
+
+describe("/api/cars", () => {
+
+  let fuel, type, make, model, user, token, url, vin, data, year, car;
+  const dataTypes = [0, null, false, undefined, ""];
+
+  // Create car with given VIN
+  const createCar = vin => Car({vin, make, model, year, fuel, type}).save();
+
+});
+
