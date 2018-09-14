c5117c581dcd243943e78624f87aac6fd97560f5
DudkinON <dudkin.on@gmail.com>
2018-09-14 15:50:45 -0500

    feat: Add test cases for the GET on /api/cars endpoint

--- tests/integration/routes/cars.test.js
+++ tests/integration/routes/cars.test.js
@@ -46,5 +46,71 @@ describe("/api/cars", () => {
     done();
   });
 
+  describe("GET /", () => {
+
+    let car2, car3;
+
+    // Prepare and return request promise
+    const prepare = () => request(server).get(url).set("x-auth-token", token);
+
+    beforeEach(async done => {
+      /**
+       * Before each test: define url and create cars
+       */
+
+      url = "/api/cars";
+
+      car = await createCar(vin);
+      car2 = await createCar("WBA5A5C51FD520400");
+      car3 = await createCar("WBA5A5C51FD520401");
+
+      done();
+    });
+
+    afterEach(async done => {
+      /**
+       * After each test remove cars
+       */
+
+      await car.remove();
+      await car2.remove();
+      await car3.remove();
+      done();
+    });
+
+    it("should return status code 403 if user isn't admin", async done => {
+
+      user.su = false;
+      await user.save();
+      const _token = await user.generateAuthToken();
+      const res = await request(server).get(url).set("x-auth-token", _token);
+
+      expect(res.status).toBe(403);
+      expect(res.body).toHaveProperty("error");
+
+      user.su = true;
+      await user.save();
+      done();
+    });
+
+    it("should return status code 200 and list of cars if GET parameters do not given", async done => {
+      const res = await prepare();
+
+      expect(res.status).toBe(200);
+      expect(res.body.length >= 3).toBeTruthy();
+      done();
+    });
+
+    it("should return status code 200 and list of cars if GET parameters defined", async done => {
+
+      const amount = 2;
+      url = `/api/cars?page=1&amount=${amount}`;
+      const res = await prepare();
+
+      expect(res.status).toBe(200);
+      expect(res.body.length === amount).toBeTruthy();
+      done();
+    });
+  });
 });
 
