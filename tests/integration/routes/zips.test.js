83cb1ed4314f03f62b4d9e275d340ce239c64cde
DudkinON <dudkin.on@gmail.com>
2018-09-17 04:25:38 -0500

    feat: Add tests for POST /api/zip endpoint

--- tests/integration/routes/zips.test.js
+++ tests/integration/routes/zips.test.js
@@ -104,4 +104,214 @@ describe("/api/zips", () => {
       done();
     });
   });
+
+  describe("POST /", () => {
+
+    let postData;
+
+    // Prepare and return POST request on url with auth token (Promise)
+    const prepare = () => request(server).post(url).set("x-auth-token", token).send(postData);
+
+    beforeEach(async done => {
+      /**
+       * Before each test define post data and url
+       * @type {{_id: number, city: string, state: string, pop: number, loc: number[]}}
+       */
+
+      postData = {
+        _id: 78701,
+        city: city.name,
+        state: state.name,
+        pop: 3857,
+        loc : [ -97.742559, 30.271289 ]};
+      url = "/api/zips";
+      done();
+    });
+
+    afterEach(async done => {
+      /**
+       * After each test remove the zip
+       */
+
+      await Zip.findByIdAndRemove(data._id);
+      done();
+    });
+
+    it("should return status code 400 if post data doesn't have loc", async done => {
+
+      delete postData.loc;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 400 if post data loc have more than 2 coordinates", async done => {
+
+      postData.loc.push(100.001);
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 400 if post data loc have less than 2 coordinates", async done => {
+
+      postData.loc.pop();
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 401 if user isn't logged in", async done => {
+
+      const res = await request(server).post(url).send(postData);
+
+      expect(res.status).toBe(401);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 403 if user isn't admin", async done => {
+
+      user.su = false;
+      await user.save();
+      const _token = await user.generateAuthToken();
+      const res = await request(server).post(url).set("x-auth-token", _token).send(postData);
+
+      expect(res.status).toBe(403);
+      expect(res.body).toHaveProperty("error");
+
+      user.su = true;
+      await user.save();
+
+      done();
+    });
+
+    it("should return status code 400 if post data loc coordinates isn't integers", async done => {
+
+      dataTypes.forEach(async item => {
+
+        postData.loc[0] = item;
+
+        const res = await prepare();
+
+        expect(res.status).toBe(400);
+        expect(res.body).toHaveProperty("error");
+
+        postData.loc[1] = item;
+
+        const res2 = await prepare();
+
+        expect(res2.status).toBe(400);
+        expect(res2.body).toHaveProperty("error");
+      });
+
+      done();
+    });
+
+    it("should return status code 400 if post data doesn't have pop", async done => {
+
+      delete postData.pop;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 400 if population is invalid", async done => {
+
+      dataTypes.forEach(async item => {
+        if (item !== 0) {
+          postData.pop = item;
+
+          const res = await prepare();
+
+          expect(res.status).toBe(400);
+          expect(res.body).toHaveProperty("error");
+        }
+      });
+
+      done();
+    });
+
+    it("should return status code 404 if state doesn't exist", async done => {
+
+      postData.state = "fakeState";
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 404 if city doesn't exist", async done => {
+
+      postData.city = "fakeCity";
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 400 if post data doesn't include a city name", async done => {
+
+      delete postData.city;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 400 if post data doesn't include a state name", async done => {
+
+      delete postData.state;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 200 and zip info if post data is valid", async done => {
+
+      const res = await prepare();
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("_id", postData._id);
+      expect(res.body).toHaveProperty("city", String(city._id));
+      expect(res.body).toHaveProperty("state", String(state._id));
+      expect(res.body).toHaveProperty("pop", postData.pop);
+      expect(res.body).toHaveProperty("loc");
+      expect(res.body.loc.length === 2).toBeTruthy();
+      expect(res.body.loc[0] === postData.loc[0]).toBeTruthy();
+      expect(res.body.loc[1] === postData.loc[1]).toBeTruthy();
+
+      done();
+    });
+  });
 });
