d9d1b2df62750b3049a6b522c8f0f4212bc9a479
DudkinON <dudkin.on@gmail.com>
2018-09-16 23:30:04 -0500

    feat: Add getByVIN method to car controller

--- controllers/cars.js
+++ controllers/cars.js
@@ -26,4 +26,43 @@ controller.getById = async (req, res) => {
   return res.send(car);
 };
 
+
+controller.getByVIN = async (req, res) => {
+
+  const { error } = validateVIN(req.params);
+  if (error) return res.status(400).send({error: error.details[0].message});
+
+  const vin = req.params.vin.toUpperCase();
+
+  // Try to find a car
+  const car = await Car.getByVIN(vin);
+  if (car) return res.send(car);
+
+  // Decoding the VIN
+  nhtsa.decodeVinFlatFormat(vin)
+    .then(async ({ data }) => {
+
+      // Retrieve data
+      if (!data.Results[0]) return res.status(500).send({error: "Server unavailable, try it later"});
+      const info = data.Results[0];
+
+      // Create a new car
+      await Car.create({
+        vin: vin,
+        make: info.Make,
+        model: info.Model,
+        type: info.BodyClass,
+        fuel: info.FuelTypePrimary,
+        year: Number(info.ModelYear)
+      });
+
+      // Send the car object to the client
+      return res.status(201).send(await Car.getByVIN(vin));
+    })
+    .catch(e => {
+      console.log(e);
+      return res.status(500).send({error: "Cannot get the car with given VIN number"});
+    });
+};
+
 module.exports = controller;
