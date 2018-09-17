cdb28fa1f5b902ce2b2867270b7e18250e33aa96
DudkinON <dudkin.on@gmail.com>
2018-09-17 04:24:32 -0500

    feat: Add tests for /api/zips/:id endpoint

--- tests/integration/routes/zips.test.js
+++ tests/integration/routes/zips.test.js
@@ -35,4 +35,73 @@ describe("/api/zips", () => {
     await user.remove();
     done();
   });
+
+  describe("GET /:id", () => {
+
+    // Prepare and return GET request on url (Promise)
+    const prepare = () => request(server).get(url).set("x-auth-token", token);
+
+    beforeAll(async done => {
+      /**
+       * Before all tests define data and create a zip
+       * @type {{_id: number, city: string, state: string, pop: number, loc: number[]}}
+       */
+
+      data = {
+        _id: 78701,
+        city: city._id,
+        state: state._id,
+        pop: 3857,
+        loc : [ -97.742559, 30.271289 ]};
+      zip = await new Zip(data).save();
+      done();
+    });
+
+    afterAll(async done => {
+      /**
+       * After all tests remove the zip
+       */
+
+      await zip.remove();
+      done();
+    });
+
+    it("should return status code 404 if zip invalid", async done => {
+
+      url = "/api/zips/wrongZip";
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
+
+    it("should return status code 404 if zip doesn't exist", async done => {
+
+      url = "/api/zips/00000";
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+      done();
+    });
+
+    it("should return status code 200 and zip if zip code is valid", async done => {
+
+      url = `/api/zips/${zip._id}`;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("_id", zip._id);
+      expect(res.body).toHaveProperty("city", String(zip.city));
+      expect(res.body).toHaveProperty("state", String(zip.state));
+      expect(res.body).toHaveProperty("loc");
+      expect(res.body.loc.length === 2).toBeTruthy();
+      expect(res.body).toHaveProperty("pop", zip.pop);
+      done();
+    });
+  });
 });
