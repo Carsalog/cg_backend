f114928f1850ea9df4f7364a887da9a8665fdb36
DudkinON <dudkin.on@gmail.com>
2018-09-17 04:23:23 -0500

    docs: Add docs to methods of tests suit for zip router

--- tests/integration/routes/zips.test.js
+++ tests/integration/routes/zips.test.js
@@ -7,12 +7,17 @@ const utils = require("./utils");
 
 
 describe("/api/zips", () => {
+  /**
+   * Test cases for /api/zips endpoint
+   */
 
   let user, token, state, city, zip, url, data;
   const dataTypes = [0, null, false, undefined, ""];
 
   beforeAll(async done => {
-
+    /**
+     * Before all tests create: state, city, and user, generate auth token
+     */
     user = await utils.createUser("john.doe@zips.test", true);
     token = await user.generateAuthToken();
     state = await new State({name: "texas", abbreviation: "TX"}).save();
@@ -22,7 +27,9 @@ describe("/api/zips", () => {
   });
 
   afterAll(async done => {
-
+    /**
+     * After all tests remove: the city, the state, and the user
+     */
     await city.remove();
     await state.remove();
     await user.remove();
