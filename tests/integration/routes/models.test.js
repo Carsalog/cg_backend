4e6cb98aa9e3c2561639214956d8a0e031c300b7
DudkinON <dudkin.on@gmail.com>
2018-09-20 14:36:04 -0500

    feat: Add test cases for GET on /by/make/:id endpoint

--- tests/integration/routes/models.test.js
+++ tests/integration/routes/models.test.js
@@ -37,4 +37,72 @@ describe("/api/models", () => {
     await done();
   });
 
+  describe("GET /by/make/:id", () => {
+
+    let x3, x4;
+
+    // Prepare and return GET request on url (Promise)
+    const prepare = () => request(server).get(url);
+
+    beforeEach(async done => {
+      /**
+       * Before each test create 2 models and define url
+       */
+
+      x3 = await Model({name: "x3", make: make._id}).save();
+      x4 = await Model({name: "x4", make: make._id}).save();
+
+      url = `/api/models/by/make/${make._id}`;
+
+      make.models.push(x3);
+      make.models.push(x4);
+      await make.save();
+      done();
+    });
+
+    afterEach(async done => {
+      /**
+       * After each test remove the models
+       */
+
+      await x3.remove();
+      await x4.remove();
+      done();
+    });
+
+    it("should return status code 404 if pass invalid make id", async done => {
+
+      url = `/api/models/by/make/1`;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 404 if make id doesn't exist", async done => {
+
+      url = `/api/models/by/make/${utils.getRandomId()}`;
+
+      const res = await prepare();
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+
+      done();
+    });
+
+    it("should return status code 200 and list of models", async done => {
+
+      const res = await prepare();
+
+      expect(res.status).toBe(200);
+      expect(res.body.length >= 2).toBeTruthy();
+
+      done();
+    });
+  });
+
 });
