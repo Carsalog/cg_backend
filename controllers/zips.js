9a52c945b65470325e8cfb4b41f4312590ec2866
DudkinON <dudkin.on@gmail.com>
2018-09-24 23:05:32 -0500

    feat: Add get method to zip controller

--- controllers/zips.js
+++ controllers/zips.js
@@ -6,4 +6,13 @@ const {City} = require("../models/cities");
 const _ = require("lodash");
 const controller = {};
 
+
+controller.get = async (req, res) => {
+
+  const zip = await Zip.findById(req.params.id);
+  if (!zip) return res.status(404).send({error: "Cannot find this zip code"});
+
+  return res.send(_.pick(zip, ["_id", "city", "state", "loc", "pop"]));
+};
+
 module.exports = controller;
