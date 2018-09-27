43d168e30214d9dd21f82a6bafa64e1956730f10
DudkinON <dudkin.on@gmail.com>
2018-09-26 23:29:55 -0500

    feat: Add post controller

---
+++ controllers/posts.js
@@ -0,0 +1,11 @@
+const {Post} = require("../models/posts");
+const {User} = require("../models/users");
+const {State} = require("../models/states");
+const {City} = require("../models/cities");
+const {Make} = require("../models/makes");
+const {Model} = require("../models/models");
+const _ = require("lodash");
+const controller = {};
+
+
+module.exports = controller;
