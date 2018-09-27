4659681d095fdee7961d1a5467b1aa69d8794f75
DudkinON <dudkin.on@gmail.com>
2018-09-26 23:30:49 -0500

    feat: Add get method to post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,4 +8,28 @@ const _ = require("lodash");
 const controller = {};
 
 
+controller.get = async (req, res) => {
+
+  let make = null, model = null;
+  const data = req.params;
+
+  if (!data.city) return res.status(400).send({error: "Cannot read city"});
+  if (!data.state) return res.status(400).send({error: "Cannot read state"});
+
+  const state = await State.getByName(data.state);
+  if (!state) return res.status(404).send({error: "Cannot find this state"});
+
+  const city = await City.getByName(data.city, state._id);
+  if (!city) return res.status(404).send({error: "Cannot find this city"});
+
+  if (data.make) make = await Make.getByName(data.make);
+  if (data.model) model = await Model.getByName(data.model, make._id);
+
+  if (data.make && !make) return res.status(404).send({error: "Cannot find this make"});
+  if (data.model && !model) return res.status(404).send({error: "Cannot find this model"});
+
+  return res.send(await Post.getByPage(
+    data.page, data.amount, city._id, state._id, make, model, data.yearMin, data.yearMax));
+};
+
 module.exports = controller;
