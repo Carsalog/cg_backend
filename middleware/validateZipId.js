0e81e5db170bd78f15ca0843a49895b3fd0cf696
DudkinON <dudkin.on@gmail.com>
2018-09-30 23:08:09 -0500

    docs: Add a docstring to validateZipId middleware

--- middleware/validateZipId.js
+++ middleware/validateZipId.js
@@ -2,6 +2,9 @@ const {isInt} = require("../lib/tools");
 const config = require("config");
 
 module.exports = function (req, res, next) {
+  /**
+   * Zip id validator middleware. Checks that id is valid, else return 404 error
+   */
 
   if (!isInt(req.params.id) || config.get("zips.id.min") > Number(req.params.id) > config.get("zips.id.max"))
     return res.status(404).send({error: "Cannot find this zip"});
