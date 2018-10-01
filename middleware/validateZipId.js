a9d047809cc1d59c5a9ab66353833b69b5dd8458
DudkinON <dudkin.on@gmail.com>
2018-09-30 23:07:08 -0500

    feat: Add validateZipId middleware

---
+++ middleware/validateZipId.js
@@ -0,0 +1,10 @@
+const {isInt} = require("../lib/tools");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+
+  if (!isInt(req.params.id) || config.get("zips.id.min") > Number(req.params.id) > config.get("zips.id.max"))
+    return res.status(404).send({error: "Cannot find this zip"});
+
+  next();
+};
