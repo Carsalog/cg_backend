b4cb82850fc496ccdac3319c3b9ea557ae7eb1ed
DudkinON <dudkin.on@gmail.com>
2018-10-02 00:50:18 -0500

    docs: Add a docstring to validatePostsUpdate middleware

--- middleware/validatePostsUpdate.js
+++ middleware/validatePostsUpdate.js
@@ -5,6 +5,9 @@ const {State} = require("../models/states");
 const {City} = require("../models/cities");
 
 module.exports = async function (req, res, next) {
+  /**
+   * Id validator middleware. Checks that id is valid, else return 404 error
+   */
 
   if (!(await State.getById(req.body.state))) return res.status(404).send({error: "Cannot find this state"});
   if (!(await City.getById(req.body.city))) return res.status(404).send({error: "Cannot find this city"});
