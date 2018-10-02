eeafb08a2a185232e7ad63e6327f77fc2a5f046a
DudkinON <dudkin.on@gmail.com>
2018-10-02 00:49:44 -0500

    feat: Add validatePostsUpdate middleware

---
+++ middleware/validatePostsUpdate.js
@@ -0,0 +1,27 @@
+const {User} = require("../models/users");
+const {Post} = require("../models/posts");
+const {Transmission} = require("../models/transmission");
+const {State} = require("../models/states");
+const {City} = require("../models/cities");
+
+module.exports = async function (req, res, next) {
+
+  if (!(await State.getById(req.body.state))) return res.status(404).send({error: "Cannot find this state"});
+  if (!(await City.getById(req.body.city))) return res.status(404).send({error: "Cannot find this city"});
+  if (!(await Transmission.getById(req.body.transmission)))
+    return res.status(404).send({error: "Cannot find this transmissions type"});
+
+  const user = await User.getById(req.user._id);
+  if (!user) return res.status(404).send({error: "Cannot find this user"});
+
+  const post = await Post.getById(req.params.id);
+  if (!post) return res.status(404).send({error: "Cannot find this post"});
+
+  // If user isn't author and isn't admin return error message
+  if (!user.su && user._id !== post.author._id) return res.status(403).send({error: "You cannot edit this post"});
+
+  const city = await City.getById(req.body.city);
+  if (String(city.state) !== req.body.state) return res.status(400).send({error: "City and state doesn't match"});
+
+  next();
+};
