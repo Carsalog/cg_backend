3672d39db80f13f4abdad8aa32b3fdf6ac758ed5
DudkinON <dudkin.on@gmail.com>
2018-10-05 22:01:12 -0500

    feat: Add winston logging

---
+++ startup/logging.js
@@ -0,0 +1,19 @@
+const winston = require("winston");
+// require("winston-mongodb");
+require("express-async-errors");
+
+module.exports = function () {
+  // Handling uncaught exceptions
+  winston.exceptions.handle(
+    new winston.transports.Console({colorize: true, prettyPrint: true}),
+    new winston.transports.File({filename: "handleException.log"}));
+
+  // Handling unhandled rejection
+  process.on("unhandledRejection", (error) => {
+    throw error
+  });
+
+  // Handling app error exceptions
+  winston.add(new winston.transports.File({filename: "logfile.log"}));
+  // winston.add(new winston.transports.MongoDB({db: config.get("db")}));
+};
