2b14b409777367d496500d0dce0e91984f12de92
DudkinON <dudkin.on@gmail.com>
2019-03-15 13:06:00 -0700

    refactor: Update validatePostsUpdate middleware

--- middleware/validatePostsUpdate.js
+++ middleware/validatePostsUpdate.js
@@ -9,8 +9,10 @@ module.exports = async function (req, res, next) {
    * Id validator middleware. Checks that id is valid, else return 404 error
    */
 
-  if (!(await State.getById(req.body.state))) return res.status(404).send({error: "Cannot find this state"});
-  if (!(await City.getById(req.body.city))) return res.status(404).send({error: "Cannot find this city"});
+  if (!(await State.getById(req.body.state)))
+    return res.status(404).send({error: "Cannot find this state"});
+  if (!(await City.getById(req.body.city)))
+    return res.status(404).send({error: "Cannot find this city"});
   if (!(await Transmission.getById(req.body.transmission)))
     return res.status(404).send({error: "Cannot find this transmissions type"});
 
@@ -21,10 +23,12 @@ module.exports = async function (req, res, next) {
   if (!post) return res.status(404).send({error: "Cannot find this post"});
 
   // If user isn't author and isn't admin return error message
-  if (!user.su && user._id !== post.author._id) return res.status(403).send({error: "You cannot edit this post"});
+  if (!user.su && String(user._id) !== String(post.author._id))
+    return res.status(403).send({error: "You cannot edit this post"});
 
   const city = await City.getById(req.body.city);
-  if (String(city.state) !== req.body.state) return res.status(400).send({error: "City and state doesn't match"});
+  if (String(city.state) !== req.body.state)
+    return res.status(400).send({error: "City and state doesn't match"});
 
   next();
 };
