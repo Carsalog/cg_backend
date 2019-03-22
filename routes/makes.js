1dbf964e747dae645f5f4f9c168fd0798fe1e2c6
DudkinON <dudkin.on@gmail.com>
2019-03-21 17:28:24 -0700

    feat: Add queryValidator middleware to get method of makes router

--- routes/makes.js
+++ routes/makes.js
@@ -6,9 +6,10 @@ const su = require("../middleware/admin");
 const idValidator = require("../middleware/idValidator");
 const _ = require("lodash");
 const valid = require("../middleware/valid");
+const queryValidator = require("../middleware/queryValidator");
 
 
-router.get("/", async (req, res) => {
+router.get("/", queryValidator, async (req, res) => {
   /**
    * Get amount of car makes by page
    * @return Object:
@@ -23,7 +24,7 @@ router.get("/by/name/:name", async (req, res) => {
    * @return Object:
    */
 
-  const { error } = validateName({name: req.params.name});
+  const {error} = validateName({name: req.params.name});
   if (error) return res.status(400).send({error: error.details[0].message});
 
   const item = await Make.getByName(req.params.name);
@@ -77,14 +78,13 @@ router.delete("/:id", [auth, su, idValidator], async (req, res) => {
    * @return Object:
    */
 
-  // Try to find the car make
+    // Try to find the car make
   const item = await Make.delete(req.params.id);
   if (!item) return res.status(404).send({error: "Cannot find this make"});
 
   // Send response to a client
   return res.send({info: `Make ${item.name} was removed`});
 });
-
 
 
 module.exports = router;
